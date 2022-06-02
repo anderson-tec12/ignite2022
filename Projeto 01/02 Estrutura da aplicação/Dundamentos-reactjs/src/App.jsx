98a3bb0a7c7dc1df64b8cd8d0d40b26725c3bff5
Anderson B. Silva <anderson.tec12@gmail.com>
2022-06-02 09:23:19 -0300

    final da sessão

---
+++ Projeto 01/02 Estrutura da aplicação/Dundamentos-reactjs/src/App.jsx
@@ -0,0 +1,11 @@
+import {Post} from './Post'
+
+export function App() {
+
+  return (
+   <div>
+     <Post author="Anderson" content="teste de propriedades"/>
+    
+   </div>
+  )
+}
---
+++ Projeto 01/02 Estrutura da aplicação/Dundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props){
+  const {author,content} = props
+  return (<div>
+    
+    <strong>{author}</strong>
+    <p>{content}</p>
+  </div>)
+}
