a93af68c7d1aa0e759dac87cdd55b431267700b9
Anderson B. Silva <anderson.tec12@gmail.com>
2022-06-08 18:25:43 -0300

    avatar

--- Projeto 01/02 Estrutura da aplicação/Fundamentos-reactjs/src/components/Sidebar.jsx
+++ Projeto 01/02 Estrutura da aplicação/Fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import {PencilLine} from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
@@ -9,7 +10,8 @@ export function Sidebar(){
       src="https://images.unsplash.com/photo-1540442872948-49b05bb760e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80" alt="" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/12089780?v=4" alt="Anderson Barros Silva" />
+        <Avatar src="https://avatars.githubusercontent.com/u/12089780?v=4"/>
+        
         <strong>Anderson B. Silva</strong>
         <span>Web developer</span>
       </div>
