061af5218e6734bfa80d2c7c3074b5038c9d3e5e
Anderson B. Silva <anderson.tec12@gmail.com>
2022-06-10 09:12:15 -0300

    Criando dados para fazer a iteração

--- Projeto 01/03 Motores do react/Fundamentos-reactjs/src/App.jsx
+++ Projeto 01/03 Motores do react/Fundamentos-reactjs/src/App.jsx
@@ -5,6 +5,54 @@ import styles from './App.module.css'
 import {Sidebar} from './components/Sidebar'
 
 import './global.css'
+
+const posts = [
+  {
+    id:1,
+    author:{
+      avatarUrl:"https://github.com/anderson-tec12.png",
+      name:"Anderson Ba. Silva",
+      role:"Desenvolvedor Front-End"
+    },
+    content:[      
+          {type:'paragraph', content:'Fala galeraa 👋'},
+          {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+          {type:'link', content:'jane.design/doctorcare'}
+         
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id:2,
+    author:{
+      avatarUrl:"https://github.com/maykbrito.png",
+      name:"Mayk Brito",
+      role:"Educator rocketseat"
+    },
+    content:[      
+          {type:'paragraph', content:'Fala galeraa 👋'},
+          {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+          {type:'link', content:'jane.design/doctorcare'}
+         
+    ],
+    publishedAt: new Date('2022-05-05 20:00:00')
+  },
+  {
+    id:3,
+    author:{
+      avatarUrl:"https://github.com/diego3g.png",
+      name:"Diego",
+      role:"Educator rocketseat"
+    },
+    content:[      
+          {type:'paragraph', content:'Fala galeraa 👋'},
+          {type:'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+          {type:'link', content:'jane.design/doctorcare'}
+         
+    ],
+    publishedAt: new Date('2022-05-04 20:00:00')
+  }
+]
 export function App() {
 
   return (
@@ -15,9 +63,11 @@ export function App() {
      <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
-      
+        {
+          posts.map(post => (
+            <Post key={post.id} author={post.author} content={post.content} publisehdAt={post.publishedAt}/>
+          ))
+        }      
         </main>
      </div>
     
